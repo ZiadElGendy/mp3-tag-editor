5db993bcf37efdcd49c72520de956183532682f8
Ziad ElGendy <ziad.m.elgendy@gmail.com>
2023-08-15 13:09:21 +0300

    Add static variables to Mp3Manger

--- MP3 Tag Editor/Mp3Manager.cs
+++ MP3 Tag Editor/Mp3Manager.cs
@@ -1,9 +1,15 @@
+using Id3;
+
 namespace MP3_Tag_Editor;
 
-public class Mp3Manager
+public static class Mp3Manager
 {
-    public Mp3Manager()
+    private static string workingFilePath; 
+    private static List<Mp3> workingMp3Files;
+
+    public static void ModifyMp3(string inputFilePath)
     {
-        
+        workingFilePath = inputFilePath;
+        workingMp3Files = (List<Mp3>)Mp3FileManager.LoadMp3Files(workingFilePath);
     }
 }
