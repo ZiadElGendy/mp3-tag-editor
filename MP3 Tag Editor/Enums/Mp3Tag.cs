6fe0a799b65f95f82e480dc897c54aa542735eb1
Mazen Kamel <mazenwkamel@gmail.com>
2023-08-20 18:30:56 +0300

    Reimplement Mp3TagManager using the new library

---
+++ MP3 Tag Editor/Enums/Mp3Tag.cs
@@ -0,0 +1,29 @@
+namespace MP3_Tag_Editor.Enums;
+
+public enum Mp3Tag
+{
+    Title,
+    Subtitle,
+    Description,
+    AlbumArtists,
+    Performers,
+    PerformersRole,
+    Composers,
+    Album,
+    Comment,
+    Genres,
+    Year,
+    Track,
+    TrackCount,
+    Disc,
+    DiscCount,
+    BeatsPerMinute,
+    InitialKey,
+    Publisher,
+    ISRC,
+    RemixedBy,
+    Grouping,
+    Conductor,
+    Copyright,
+    Pictures
+}
--- MP3 Tag Editor/Program.cs
+++ MP3 Tag Editor/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System.Globalization;
+using MP3_Tag_Editor.Enums;
+using TagLib.Tiff.Arw;
 
 namespace MP3_Tag_Editor;
 
 internal static class Program
 {
-    public static readonly CultureInfo CultureInfo = new("en-UK");
     private static void Main(string[] args)
     {
-        //Sonarlint shut up
+        const string testingDirectory = @"D:\Projects\Programming\Team Projects\BMZYY\mp3-tag-editor\Sample MP3s - Copy";
+        
+        // Using string for tags
+        Mp3TagsManager.ModifyMp3Tags(testingDirectory, "album artists", "Sans", true);
+        
+        // Using enum for tags
+        Mp3TagsManager.ModifyMp3Tags(testingDirectory, Mp3Tag.Title, "Megalovania", true);
     }
 }
