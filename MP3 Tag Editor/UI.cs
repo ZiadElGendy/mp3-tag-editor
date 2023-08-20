d8dde762282b9c2a103b09511ffd24baea18d8e4
Youssef Zahran <yusef.a.zahran@gmail.com>
2023-08-20 20:46:23 +0300

    Create UI.cs and MainMenu method

---
+++ MP3 Tag Editor/UI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Mp3MP3_Tag_Editor;
+
+public static class UI
+{
+    public static MainMenu()
+    {
+        console.WriteLine("Welcome to MP3 Tag Editor\n" +
+            "Enter h for help\n" +
+            "Enter e to enter MP3 modification\m" +
+            "Enter x to exit program\n");
+        console.ReadLine
+    }
+
+
+}
