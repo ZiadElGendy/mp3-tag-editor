03edb8911cfe06ee0460654b3c0a3f67ee034471
Ziad ElGendy <ziad.m.elgendy@gmail.com>
2023-08-23 18:51:09 +0300

    Update Main loop

--- MP3 Tag Editor/Program.cs
+++ MP3 Tag Editor/Program.cs
@@ -2,8 +2,27 @@
 
 internal static class Program
 {
+    private static bool _exit = false;
     private static void Main(string[] args)
     {
-        // Sonarlint shut up        
+        while (!_exit)
+        {
+            var input = Ui.MainMenu();
+            switch (input)
+            {
+                case "h":
+                    Ui.HelpMenu();
+                    break;
+                case "e":
+                    Ui.ModifyMp3Menu();
+                    break;
+                case "x":
+                    _exit = true;
+                    break;
+                default:
+                    Console.WriteLine("Invalid input, please try again");
+                    break;
+            }
+        }
     }
 }
