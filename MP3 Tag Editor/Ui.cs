0d8587bff8033ceec918aa1b468ab8daa91252a8
Ziad ElGendy <ziad.m.elgendy@gmail.com>
2023-08-23 18:43:15 +0300

    Update HelpMenu

--- MP3 Tag Editor/Ui.cs
+++ MP3 Tag Editor/Ui.cs
@@ -1,28 +1,37 @@
-﻿using MP3_Tag_Editor;
-using System;
+﻿using MP3_Tag_Editor.Enums;
 
-namespace Mp3MP3_Tag_Editor;
+namespace MP3_Tag_Editor;
 
-public static class UI
+public static class Ui
 {
     /*current flow (in my head)
      * in a loop, main will call MainMenu()
      * then depending on 3 options, will either call HelpMenu(), Exit, or enter the ModifyMenu() Flow, which will be self contained in it's methods
      * after the modify ends, the loop starts again at MainMenu(), prompting the user to choose what to do again
      */
-    public static String MainMenu()
+    public static string MainMenu()
     {
         Console.WriteLine("Welcome to MP3 Tag Editor\n" +
             "Enter h for help\n" +
             "Enter e to enter MP3 modification\n" +
-            "Enter x to exit program\n");
-        String input = Console.ReadLine("Enter your choice:");
+            "Enter x to exit program\n" +
+            "Enter your choice:");
+
+        var input = Console.ReadLine();
         return input;
     }
 
-    public static void HelpMenu() {
+    public static void HelpMenu()
+    {
         Console.WriteLine("This is MP3 Tag Editor, a program to modify MP3 file tags.\n" +
-            "Enter the path to your Mp3 file or directory that includes Mp3 files that you wish to modify\n")
+                          "Enter the path to your Mp3 file or directory that includes Mp3 files that you wish to modify\n" +
+                          "The accepted fields to change are:\n");
+
+        foreach (Enum tag in Enum.GetValues(typeof(Mp3Tag)))
+        {
+            Console.WriteLine(tag);
+        }
+
     }
 
     public static void ModifyMp3(String filePath)
