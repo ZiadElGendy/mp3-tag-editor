45292ab83ac25eb7f0ddedfdc7a143c53b37f6df
Youssef Zahran <yusef.a.zahran@gmail.com>
2023-08-23 15:28:27 +0300

    Updated MainMenu and HelpMenu methods in UI.cs

--- MP3 Tag Editor/UI.cs
+++ MP3 Tag Editor/UI.cs
@@ -6,11 +6,11 @@ public static class UI
 {
     public static MainMenu()
     {
-        console.WriteLine("Welcome to MP3 Tag Editor\n" +
+        Console.WriteLine("Welcome to MP3 Tag Editor\n" +
             "Enter h for help\n" +
             "Enter e to enter MP3 modification\m" +
             "Enter x to exit program\n");
-        console.ReadLine
+        Console.ReadLine
     }
 
 
